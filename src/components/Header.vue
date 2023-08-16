730bf44b3a389265e4d551c331315447a0368143
Jason Kirchman <kirchman.jason@icloud.com>
2023-08-16 10:11:51 -0500

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/sets">Sets</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    background-color: whitesmoke;
+    height: 2rem;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Sports Cards Wiki</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
