4c0b2ce6c79db6e083dbe52b4f43e8a159c21e4e
Jason Kirchman <kirchman.jason@icloud.com>
2023-08-18 12:26:42 -0500

    Get sets from api

--- src/router/views/SetsView.vue
+++ src/router/views/SetsView.vue
@@ -1,9 +1,38 @@
 <script setup>
-import Dropdown from '../../components/Dropdown.vue'
+import { ref, watch, onMounted } from 'vue';
+
+let allSets = null;
+const selectedSport = ref('Football');
+const selectedYear = ref(2017);
+const displaySets = ref();
+const selectedSet = ref();
+
+async function getAllSets() {
+  console.log('get sets started');
+  const res = await fetch('http://localhost:8080/sets');
+  const data = await res.json();
+  allSets = data.sets;
+  updateSets();
+  console.log('finsihed getting sets');
+}
+
+function updateSets() {
+  displaySets.value = allSets.filter((set) => {
+    return (
+      set.sport === selectedSport.value &&
+      set.year === selectedYear.value
+    );
+  });
+}
+
+onMounted(getAllSets);
 </script>
 
 <template>
-
-      <Dropdown />
-
+  <Dropdown
+    v-model="selectedSet"
+    :options="displaySets"
+    optionLabel="set"
+    placeholder="Select a Set"
+    class="w-full md:w-14rem" />
 </template>
